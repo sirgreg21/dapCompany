0982cf24b0c48cda988ada1c391d39e09eecb008
Rogério Gregório <pnrogeriogregorio@gmail.com>
2021-06-25 12:10:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+/* Abre e fecha o menu quando clicar no ícone*/
+const nav = document.querySelector('header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um ícone do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+    //função para mover rolar suavemente
+    scrollSmooth(link)
+  })
+}
+
+/* Mudar o header da página quando der scroll */
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* Testimonials carousell slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scrool na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
+
+/* Scroll suave */
+// pegar todos os itens do menu
+// ao clicar em um item rolar página suavemente
+function scrollSmooth(link) {
+  const sectionId = link.getAttibute('href')
+  document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' })
+}
